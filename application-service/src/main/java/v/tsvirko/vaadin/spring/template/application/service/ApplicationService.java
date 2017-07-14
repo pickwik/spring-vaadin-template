475f1720617981f28763601bab21b8bcb14eb787
Unknown <vladtsvirko@yandex.ru>
2017-07-14 16:59:50 +0300

    initial

---
+++ application-service/src/main/java/v/tsvirko/vaadin/spring/template/application/service/ApplicationService.java
@@ -0,0 +1,14 @@
+package v.tsvirko.vaadin.spring.template.application.service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vladtsvirko on 14.07.17.
+ */
+@SpringBootApplication
+public class ApplicationService {
+
+    public static void main(String... args){
+
+    }
+}
